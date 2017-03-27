45b543bca3852059ef80ad0efd4bd3835f416a13
Hack Reactor Student <student@hackreactor.com>
2017-03-27 15:22:16 -0700

    Passing all pomander style guides and tests for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,40 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-  if(typeof obj === 'number' || typeof obj === 'boolean' || obj === null ){
-  	return '' + obj;
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null ) {
+    return '' + obj;
   }
-  if(typeof obj === 'string'){
-  	return '"' + obj + '"';
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
   }
-  if(obj === undefined){
-  	return undefined;
+  if (obj === undefined) {
+    return undefined;
   }
-  if(typeof obj === 'function'){
-  	return null;
+  if (typeof obj === 'function') {
+    return null;
   }
-  if(Array.isArray(obj)){
-  	var result = [];
-  	if(obj.length === 0){
-  		return '[]';
-  	}
-  	else{
-  		for(var i = 0; i < obj.length; i++){
-          result.push(stringifyJSON(obj[i]));
-  		} 	 	
-  	}
-  	return '[' + result + ']';
+  if (Array.isArray(obj)) {
+    var result = [];
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      for (var i = 0; i < obj.length; i++) {
+        result.push(stringifyJSON(obj[i]));
+      } 	 	
+    }
+    return '[' + result + ']';
   }
-  if(typeof obj === 'object'){
-  	var resultObject = [];
-  	if(Object.keys(obj).length === 0 || obj[Object.keys(obj)[0]] === undefined || typeof obj[Object.keys(obj)[0]] === 'function'){
-  		return '{}';
-  	}
-  	else{
-  	for(var key in obj){
-  		resultObject.push([stringifyJSON(key)] + ':' + stringifyJSON(obj[key]));
-  	}
-  	return '{' + resultObject.join(',') + '}';
+  if (typeof obj === 'object') {
+    var resultObject = [];
+    if (Object.keys(obj).length === 0 || obj[Object.keys(obj)[0]] === undefined || typeof obj[Object.keys(obj)[0]] === 'function') {
+      return '{}';
+    } else {
+      for (var key in obj) {
+        resultObject.push([stringifyJSON(key)] + ':' + stringifyJSON(obj[key]));
+      }
+      return '{' + resultObject.join(',') + '}';
+    }
   }
- }
 };
