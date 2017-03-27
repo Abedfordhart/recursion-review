008eb14b8a073736f74b41f8f7774417a89386c6
Hack Reactor Student <student@hackreactor.com>
2017-03-27 16:42:33 -0700

    Made progress for parseJson

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,14 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+ 
+  if (json.charAt(0) === '[') {
+    var jsonArray = json.split(',');
+    return jsonArray;
+  }
+  if (json.charAt(0) === '{') {
+
+
+  }
 };
+
